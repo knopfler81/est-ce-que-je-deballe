c4fbebd7ccac9a242e8f75d284de2cc7d6e35c39
Nelly Duclos <knopfler42@yahoo.fr>
2017-04-16 16:00:30 +0200

    added api forecastio

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,4 +1,16 @@
 class Market < ApplicationRecord
   geocoded_by :address   # can also be an IP address
   after_validation :geocode          # auto-fetch coordinates
+
+
+  def temperature
+    json = ForecastIO.forecast(latitude, longitude, params: { units: 'si' })
+    json.currently.temperature.to_i
+  end
+
+  def wind
+    json = ForecastIO.forecast(latitude, longitude, params: { units: 'si' })
+    json.currently.windSpeed * 1.609344
+  end
+
 end
